73672a54b7dae5cddd7798459db6eb9bd7e3c49d
Vahid Tyagi <vahidtyagi007@gmail.com>
2026-01-29 11:40:51 +0530

    29Jan2026

--- src/main/java/base/abstract_and_interface.java
+++ src/main/java/base/abstract_and_interface.java
@@ -2,6 +2,11 @@ package base;
 
 public class abstract_and_interface {
 	
+	// abstract class vs interface
+	// abstract class can have method implementations, while interface methods are abstract by default (until Java 8).	
+	// abstract class supports single inheritance, while interface supports multiple inheritance.
+	
+	
 	
 
 }
---
+++ src/main/java/base/swapNumber.java
@@ -0,0 +1,18 @@
+package base;
+
+public class swapNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		System.out.println("Before swapping: a="+a+" b="+b);
+		a=a+b; //30
+		b=a-b; //10
+		a=a-b; //20
+		System.out.println("After swapping: a="+a+" b="+b);
+		
+
+	}
+
+}
